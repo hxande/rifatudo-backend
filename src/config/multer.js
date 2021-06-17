e1a4034b89cf3036f0c00df1b7352bffb220a7ed
Hugo <c1297097@bb.com.br>
2021-06-16 22:47:01 -0300

    Configuracao do multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,6 +6,14 @@ const multerS3 = require('multer-s3');
 
 const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024;
 
+aws.config.update({
+    credentials: {
+        accessKeyId: 'AKIAWUDM6FU2LHVM7L7E',
+        secretAccessKey: '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',
+    },
+    region: 'us-east-1',
+});
+
 const storageTypes = {
     local: multer.diskStorage({
         destination: (req, file, cb) => {
@@ -22,11 +30,6 @@ const storageTypes = {
     s3: multerS3({
         s3: new aws.S3({
             apiVersion: '2006-03-01',
-            region: 'us-east-1',
-            credentials: {
-                accessKeyId: 'AKIAWUDM6FU2LHVM7L7E',
-                secretAccessKey: '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',
-            },
         }),
         bucket: 'rifatudo',
         contentType: multerS3.AUTO_CONTENT_TYPE,
